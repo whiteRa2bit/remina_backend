2ac65d3c563300f02ffc68e301c16fb0f632fc70
whiteRa2bit <pavel.fakanov@gmail.com>
2020-08-07 23:31:17 +0300

    Add cors to settings

--- remina_backend/settings.py
+++ remina_backend/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'remina_backend.cors.AllowCors',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
